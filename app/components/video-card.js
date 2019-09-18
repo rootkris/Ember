1e23284cd9da7be7c9e48e0e124eb4a0496ade33
Kristo Roots <31840227+rootkris@users.noreply.github.com>
2019-09-18 20:41:26 +0300

    final version

---
+++ app/components/video-card.js
@@ -0,0 +1,41 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+    model: [{
+        title: "Build An Ember.js App In 2019 For Beginners",
+        people: "Program With Erik",
+        videoLink: "_JfNuppX1n8"
+    },{
+        title: "Ember.js Tutorial: Build a painting game in 20 mins",
+        people: "Gavin Joyce",
+        videoLink: "N4KrBuO0RRE"
+    },{
+        title: "How to use Ember.js Components",
+        people: "Program With Erik",
+        videoLink: "yPnCdv_s1F0"
+    },{
+        title: "Getting Started - Ember.js Guides #1",
+        people: "Gavin Joyce",
+        videoLink: "VmWSS6Ki8Qg"
+    },{
+        title: "Ember Tutorial - Ember.js Guides #2",
+        people: "Gavin Joyce",
+        videoLink: "SzGoQsUmOco"
+    },{
+        title: "A beginners guide to Ember.js Routes!",
+        people: "Program With Erik",
+        videoLink: "__Vrs3pCZhY"
+    },{
+        title: "EmberJS Framework Basics: Ember Overview",
+        people: "Envato Tuts+",
+        videoLink: "tra0-u6hLTg"
+    },{
+        title: "Ember-CLI Tutorial",
+        people: "Scott Batson",
+        videoLink: "ljLxZw-XStw"
+    },{
+        title: "Ember.js Tutorial: Build a live updating league table",
+        people: "Gavin Joyce",
+        videoLink: "vYakop2d2RI"
+    }]
+});
